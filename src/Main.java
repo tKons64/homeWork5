370f8a0d9fac414163291220697d7bfa405c4293
tKons64 <spb_unex@mail.ru>
2022-09-05 18:55:55 +0300

    Выполненны задания №1 и №2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,44 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("ghdbtn!");
+// Задание №1
+        System.out.println("");
+        System.out.println("Задание №1:");
+
+        byte clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        else {
+            System.out.println("Ошибка работы сайта! Обратитесь к администратору");
+        }
+
+// Задание №2
+        System.out.println("");
+        System.out.println("Задание №2:");
+
+        //byte clientOS = 2;
+        short clientDeviceYear = 2014;
+        if (clientOS == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else if (clientOS == 1) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        } else {
+            System.out.println("Ошибка работы сайта! Обратитесь к администратору");
+        }
+
+
+
     }
 }
